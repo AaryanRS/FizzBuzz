9c69254ff301d2f3ca9274fd620eefde4fe2ca46
AaryanRS <Ramesh.shelar897@gmail.com>
2021-10-12 11:28:47 +0530

    first commit

---
+++ Fizzbuzz.py
@@ -0,0 +1,16 @@
+a = 0
+
+while (a <= 1000) :
+    if a % 3 == 0 and a % 5 == 0:
+        print("FizzBuzz")
+
+    elif a % 3 == 0 :
+        print("Fizz")
+
+    elif a % 5 == 0 :
+        print("Buzz")
+
+    else:
+        print(a)
+
+    a += 1
